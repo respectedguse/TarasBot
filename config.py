8521792bcdcd58fc2016688cc0d7f16974f9d65a
Respected Guse <78680750+respectedguse@users.noreply.github.com>
2025-08-27 20:44:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,5 @@
 #settings bot
-TOKEN = "ODc0MjE1NzQ4Nzk5NzIxNDky.GRviH3.OHzzOlSIzgOQWvRmxE5sYxxna7xl3HcjhUYq08"
-CD = "D:\\Проекти\\UKB project\\Discord\\Taras"
 BY_LINE = "TarasBot 2024©"
-
-#bot
-invite = "https://discord.com/api/oauth2/authorize?client_id=969989335917137930&permissions=8&scope=bot"
-server_invite = "https://discord.gg/2q5kEJ3Zyh"
-
-#developer links
-bot_creator = 503904015638200340
-tg = "https://t.me/vitaliiuadev"
-
-#project links
-discord_server_project = ""
-telegram_channel_project = ""
-mono = "5375414127650769"
-donate = " https://donatello.to/ukb_project_offical"
-
-
-#links for bot
-response = "https://forms.gle/Bi2XNbcbmHaEaMYaA"
-gmail = "tarasbotukbproject@gmail.com"
-
 
 #settings
 langpr = "Python 3.12.1"
